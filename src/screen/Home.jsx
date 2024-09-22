6cfab19adabc02cb1b6927763e243514b9b450ec
nico <nico_torregrosa@hotmail.com>
2024-09-21 21:47:20 -0300

    Agregado el título y el logo del Menu Principal

--- src/screen/Home.jsx
+++ src/screen/Home.jsx
@@ -1,25 +1,15 @@
 import React from 'react'
-import Cuadrito from '../components/Cuadrito'
-
+import logo from "../assets/logo.png"
 
 
 function Home() {
   return ( 
     <div>
-      <div>
-        <h1 className='text-center m-3'>hola</h1>
-        <h1 className='text-center m-3'>hola</h1>
-        <h1 className='text-center m-3'>hola</h1>
-        <h1 className='text-center m-3'>hola</h1>
-        <h1 className='text-center m-3'>hola</h1>
-        <h1 className='text-center m-3'>hola</h1>
-        <h1 className='text-center m-3'>hola</h1>
-        <h1 className='text-center m-3'>hola</h1>
-        <h1 className='text-center m-3'>hola</h1>
-        <h1 className='text-center m-3'>hola</h1>
-        <h1 className='text-center m-3'>hola</h1>
-        <h1 className='text-center m-3'>hola</h1>
+      <div className='d-flex align-items-center justify-content-center my-5' >
+        <img src={logo} alt="Logo" style={{ width: '70px', height: '70px', marginRight: '20px' }} />
+        <h1 style={{fontFamily: "Crimson Text, serif"}} >Menú Principal</h1>
       </div>
+    
     </div>
   )
 }
